dedb085ca4aa33b9bb2b90c7347cc56e63b0eab8
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-03-21 00:42:08 +0700

    update responsive mobie && tablet HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,3 @@
-import React, { useState } from 'react'
-import classNames from 'classnames'
-import { Link } from 'react-router-dom'
-
 import { SiSublimetext } from 'react-icons/si'
 import AsideBar from './components/AsideBar'
 import VideoList from './components/VideoList'
