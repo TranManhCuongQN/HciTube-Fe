a3905a8903b25074ba2b7ca7a69249603b1a5f9f
vukhanh1209 <nguyenhongvukhanh@gmail.com>
2023-05-17 21:48:01 +0700

    update playlist on detail page

--- src/pages/DetailPage/components/PlayListFavorite/VideoItem/VideoItem.tsx
+++ src/pages/DetailPage/components/PlayListFavorite/VideoItem/VideoItem.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import { useRef, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { User } from 'src/types/user.type'
 import { Video } from 'src/types/video.type'
 import { BsPlayFill } from 'react-icons/bs'
@@ -12,9 +12,13 @@ interface VideoItemProps {
 const VideoItem = (props: VideoItemProps) => {
   const progressRef = useRef<HTMLDivElement>(null)
   const queryConfig = useQueryConfig()
-  const { favorite, category } = queryConfig
+  const location = useLocation();
 
+  const { favorite, category } = queryConfig
   const { data } = props
+  let active = false
+
+  if(location.pathname.split('/')[2] == data._id) active = true;
 
   useEffect(() => {
     const valPercent = ((data?.watchTime as number) / Number(data.duration)) * 100
@@ -35,20 +39,18 @@ const VideoItem = (props: VideoItemProps) => {
     return '00:00'
   }
 
-  const active = false
-
   return (
     <Link
       to={`/detail/${data._id}?favorite=${favorite}&category=${category || '1'}`}
       className={`${
         active ? 'bg-[rgba(0,0,0,0.05)] dark:bg-[#212121]' : ''
-      } flex  w-full cursor-pointer items-center gap-y-3 p-3 hover:bg-[rgba(0,0,0,0.05)] dark:hover:bg-[#212121]`}
+      } flex  w-full cursor-pointer items-center gap-y-3 py-3 hover:bg-[rgba(0,0,0,0.05)] dark:hover:bg-[#212121]`}
       role='presentation'
     >
       <BsPlayFill
         className={`${
-          active ? 'hidden md:flex ' : 'hidden'
-        } items-center  px-1 text-4xl text-[#606060] dark:text-[#CCB4A3] max-lg:pr-3`}
+          active ? 'opacity-1 ' : 'opacity-0'
+        } flex justify-center mx-1 items-center min-w-[24px] text-[#606060] dark:text-[#CCB4A3]`}
       />
       <div className='mr-2 aspect-video h-fit w-40 rounded-lg lg:w-24'>
         <div className='relative w-40 overflow-hidden rounded-xl lg:w-24'>
--- src/pages/DetailPage/components/Playlist/VideoItem/VideoItem.tsx
+++ src/pages/DetailPage/components/Playlist/VideoItem/VideoItem.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import { useRef, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { User } from 'src/types/user.type'
 import { Video } from 'src/types/video.type'
 import { BsPlayFill } from 'react-icons/bs'
@@ -12,9 +12,16 @@ interface VideoItemProps {
 const VideoItem = (props: VideoItemProps) => {
   const progressRef = useRef<HTMLDivElement>(null)
   const queryConfig = useQueryConfig()
+  const location = useLocation();
   const { playList, category } = queryConfig
-
+  let active = false
   const { data } = props
+
+  console.log('location',location.pathname.split('/')[2])
+
+
+  if(location.pathname.split('/')[2] == data._id) active = true;
+
 
   useEffect(() => {
     const valPercent = ((data?.watchTime as number) / Number(data.duration)) * 100
@@ -35,22 +42,21 @@ const VideoItem = (props: VideoItemProps) => {
     return '00:00'
   }
 
-  const active = false
 
   return (
     <Link
       to={`/detail/${data._id}?playList=${playList}&category=${category || '1'}`}
       className={`${
         active ? 'bg-[rgba(0,0,0,0.05)] dark:bg-[#212121]' : ''
-      } flex  w-full cursor-pointer items-center gap-y-3 p-3 hover:bg-[rgba(0,0,0,0.05)] dark:hover:bg-[#212121]`}
+      } flex  w-full cursor-pointer items-center gap-y-3 py-3 hover:bg-[rgba(0,0,0,0.05)] dark:hover:bg-[#212121]`}
       role='presentation'
     >
       <BsPlayFill
         className={`${
-          active ? 'hidden md:flex ' : 'hidden'
-        } items-center  px-1 text-4xl text-[#606060] dark:text-[#CCB4A3] max-lg:pr-3`}
+          active ? 'opacity-1 ' : 'opacity-0'
+        } flex justify-center mx-1 items-center min-w-[24px] text-[#606060] dark:text-[#CCB4A3]`}
       />
-      <div className='mr-2 aspect-video h-fit w-40 rounded-lg lg:w-24'>
+      <div className='mr-2 aspect-video h-fit w-40 rounded-lg lg:w-24 '>
         <div className='relative w-40 overflow-hidden rounded-xl lg:w-24'>
           <img src={data?.thumbnail} alt='thumbnail' className='aspect-video w-40 rounded-md object-cover lg:w-24 ' />
           <span className='absolute right-1 bottom-1 rounded-sm bg-[rgba(0,0,0,0.8)] px-1 text-xs font-bold text-slate-200'>
@@ -62,7 +68,7 @@ const VideoItem = (props: VideoItemProps) => {
 
       <div className='flex items-start gap-x-3 md:w-full'>
         <div className='flex flex-col '>
-          <span className='pr-6 text-sm font-semibold text-black line-clamp-2 dark:text-white '>{data?.title}</span>
+          <span className='pr-10 text-sm font-semibold text-black line-clamp-2 whitespace-normal dark:text-white'>{data?.title}</span>
           <div className='text-xs font-normal text-gray-500 dark:text-gray-400 '>
             {`${(data?.channel as User).fullName}`}
           </div>
