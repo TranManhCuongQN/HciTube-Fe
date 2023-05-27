b84370a0db6429eca4fe8d89281143297c9b624b
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-05-27 22:24:34 +0700

    fix url

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  baseUrl: 'http://localhost:3000/',
+  baseUrl: 'https://hcitube-api.onrender.com',
   ClOUD_NAME: 'dj5es0etq',
   PRESENT_NAME: 'fsribkzz',
   FOLDER_NAME_VIDEO: 'youtube-clone/video',
